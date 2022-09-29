817aeb3e5f5abf386594ab68c024aa69e2d8ee93
ChaudhariNaresh <nareshchaudhari@appsly.in>
2022-09-29 18:38:03 +0530

    complete home

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,11 @@
 import Head from 'next/head';
 import Image from 'next/image';
+import About from '../components/About';
+import Contact from '../components/Contact';
 import Main from '../components/Main';
 import Navbar from '../components/Navbar';
+import Projects from '../components/Projects';
+import Skills from '../components/Skills';
 
 export default function Home() {
   return (
@@ -13,6 +17,10 @@ export default function Home() {
       </Head>
       <Navbar />
       <Main />
+      <About />
+      <Skills />
+      <Projects />
+      <Contact />
     </div>
   );
 }
